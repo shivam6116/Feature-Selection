93efbff368dd1146505ee9a5022fc37038456e99
shivam6116 <shivammishra692002@gmail.com>
2024-07-20 17:07:35 +0530

    updated the data loader class

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,3 +1,4 @@
+'''Data Loader Class'''
 import os
 import pandas as pd
 import pyarrow.parquet as pq
@@ -5,6 +6,7 @@ import logging
 import yaml
 
 class DataLoader:
+    '''Data Loader Class'''
     def __init__(self, config_path):
         self.config = self._load_config(config_path)
         self.parquet_dir = self.config['data']['parquet_dir']
@@ -12,27 +14,37 @@ class DataLoader:
         self.cat_var = self.config['data']['cat_var']
         self._setup_logging()
 
-    def _load_config(self, config_path):
-        with open(config_path, 'r') as file:
+
+    def _load_config(self, config_path :str) -> dict:
+        '''Loads the configuration file and returns it as a dictionary'''
+        with open(config_path, 'r',encoding='utf-8') as file:
             config = yaml.safe_load(file)
         return config
 
-    def _setup_logging(self):
+
+    def _setup_logging(self)-> None:
+        '''Sets up the logging configuration'''
         logging.basicConfig(filename=self.config['logging']['filename'],
                             level=self.config['logging']['level'],
                             format='%(asctime)s:%(levelname)s:%(message)s')
 
-    def load_data(self):
+
+    def load_dataset(self)-> pd.DataFrame:
+        '''Loads the parquet dataset from directory and returns it as a pandas dataframe'''
+
         if not os.path.exists(self.parquet_dir):
-            logging.error(f"Directory not found: {self.parquet_dir}")
+            logging.error("Directory not found: %s",self.parquet_dir)
             raise FileNotFoundError(f"Directory not found: {self.parquet_dir}")
 
         dataset = pq.ParquetDataset(self.parquet_dir)
         df = dataset.read_pandas().to_pandas()
-        logging.info(f"Loaded data from {self.parquet_dir}")
+        logging.info("Loaded data from %s",self.parquet_dir)
+
         return df
 
-    def drop_system_columns(self, df):
+    def drop_system_columns(self, df)-> pd.DataFrame:
+        '''Drops the system columns from the dataframe'''
+
         df = df.drop(self.sys_col, axis=1)
         logging.info("Dropped system columns")
         return df
