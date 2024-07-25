71b5c588038389f3f58095d3344f197b25f6a0bc
shivam6116 <shivammishra692002@gmail.com>
2024-07-25 09:49:08 +0530

    updated segment

--- src/feature_processor.py
+++ src/feature_processor.py
@@ -74,18 +74,15 @@ class Featureprocessor:
         return df
 
 
-    def merge_dataframes(self, left_df, right_df, join_column, join_type="left"):
+    def merge_dataframes(self,
+                         left_df:pd.DataFrame,
+                         right_df:pd.DataFrame,
+                         join_column:list,
+                         join_type="left")->pd.DataFrame:
         """Merges two DataFrames on the specified column(s) using the specified join type."""
-        if not isinstance(left_df, pd.DataFrame):
-            raise ValueError("left_df must be a pandas DataFrame")
-        if not isinstance(right_df, pd.DataFrame):
-            raise ValueError("right_df must be a pandas DataFrame")
-        if not isinstance(join_column, (str, list)):
-            raise ValueError("join_column must be a string or a list of strings")
-        if not isinstance(join_type, str):
-            raise ValueError("join_type must be a string")
-
+        
         merged_df = pd.merge(left_df, right_df, how=join_type, on=join_column)
+        merged_df= self.drop_selective_columns(merged_df, join_column)
         logging.info("Data Frames merged successfully")
 
         return merged_df
--- src/utils/segments.py
+++ src/utils/segments.py
@@ -1,10 +1,16 @@
+'''Dataset Segmentation'''
+
 import numpy as np
 import pandas as pd
 
 class Segmenter:
-    @staticmethod
-    def create_segments(pd_df):
-        commuteDist75th = pd_df['avg_site_dist_day_night'].quantile(0.75)
+    '''Segmenter Class'''
+
+    def calculate_quantile(self, df: pd.DataFrame, column_name: str, quantile: float=0.75) -> pd.DataFrame:
+        '''Creates segments using quantiles'''
+        return df[column_name].quantile(quantile)
+    
+'''commuteDist75th = pd_df['avg_site_dist_day_night'].quantile(0.75)
         commuteDiv75th = pd_df['location_diversity_index'].quantile(0.75)
 
         pd_df["HighTraveller"] = np.where(
@@ -12,7 +18,7 @@ class Segmenter:
             (pd_df['location_diversity_index'] >= commuteDiv75th), 1, 0
         )
 
-        return pd_df
+        return pd_df'''
 
     @staticmethod
     def save_segment_stats(pd_df, config):
@@ -25,3 +31,22 @@ class Segmenter:
         nonSegment.describe().to_csv(config['data']['output_files']['non_travel_segment_summary'])
         nonSegment.var().to_csv(config['data']['output_files']['non_travel_segment_var'])
         nonSegment.corr().to_csv(config['data']['output_files']['non_travel_corr_matrix'])
+
+    def segDefFunc():
+        rechargeEvents75th = combinedMetrics['RechargeEvents'].quantile(0.75)
+        rechargeAmt25th = combinedMetrics['RechargeAmt'].quantile(0.25)
+        
+        rsltSegment = combinedMetrics[
+            (combinedMetrics['RechargeEvents'] >= rechargeEvents75th) & 
+            (combinedMetrics['RechargeAmt'] <= rechargeAmt25th)]
+        
+    def segDefFunc2():
+        import pandas as pd
+        global combinedMetrics
+        
+        rechargeAmtThreshold = combinedMetrics['RechargeAmt'].quantile(0.80)
+        rechargeEventsThreshold = combinedMetrics['RechargeEvents'].quantile(0.80)
+        
+        rsltSegment = combinedMetrics[
+            (combinedMetrics['RechargeAmt'] >= rechargeAmtThreshold) &
+            (combinedMetrics['RechargeEvents'] >= rechargeEventsThreshold)]
